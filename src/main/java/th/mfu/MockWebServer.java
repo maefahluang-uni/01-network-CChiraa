17c53d2254131d78b4fe7e6cadbbd5bfff2f1fda
CChiraa <6831503902@lamduan.mfu.ac.th>
2025-08-04 17:47:59 +0700

    Update MockWebServer.java

--- src/main/java/th/mfu/MockWebServer.java
+++ src/main/java/th/mfu/MockWebServer.java
@@ -1,64 +1,62 @@
 package th.mfu;
 
+import static org.junit.Assert.assertTrue;
+
+import org.junit.*;
 import java.io.*;
 import java.net.*;
 
-public class MockWebServer implements Runnable {
+/**
+ * Unit test for simple App.
+ */
+public class MockWebServerTest {
+    // Test constants
+    private static final int PORT = 8080;
+    private static String lineSeparator = System.lineSeparator();
+    private static final String TEST_RESPONSE = "HTTP/1.1 200 OK"+lineSeparator+"Content-Type: text/html"+lineSeparator+lineSeparator+"<html><body>Hello, Web! on Port "+PORT+"</body></html>"+lineSeparator;
 
-    private int port;
+    // Mock Web Server instance
+    private Thread mockWebServer;
 
-    public MockWebServer(int port) {
-        this.port = port;
+    @Before
+    public void setUp() {
+        // Start the mock web server before each test
+        mockWebServer = new Thread(new MockWebServer(PORT));
+        mockWebServer.start();
     }
 
-    @Override
-    public void run() {
-
-        // TODO Create a server socket bound to specified port
-
-        System.out.println("Mock Web Server running on port " + port + "...");
-
-        while (true) {
-            // TODO Accept incoming client connections
-
-            // TODO Create input and output streams for the client socket
-
-            // TODO: Read the request from the client using BufferedReader
-
-            // TODO: send a response to the client
-            String response = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n"
-                    + "<html><body>Hello, Web! on Port " + port + "</body></html>";
-
-            // TODO: Close the client socket
-
-        }
-
+    @After
+    public void tearDown() {
+        // Stop the mock web server after each test
+        mockWebServer.interrupt();
     }
 
-    public static void main(String[] args) {
-        Thread server1 = new Thread(new MockWebServer(8080));
-        server1.start();
+    @Test
+    public void testMockWebServer() throws IOException {
+       // Create input and output streams for the socket
+       Socket socket = new Socket("localhost", PORT);
 
-        Thread server2 = new Thread(new MockWebServer(8081));
-        server2.start();
+       PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+       BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+       
+       // Send an HTTP GET request to the web server
+       String request = "GET / HTTP/1.1"+lineSeparator+"Host: localhost"+lineSeparator+lineSeparator;
+       out.println(request);
 
-        // type any key to stop the server
-        // Wait for any key press to stop the mock web server
-        System.out.println("Press any key to stop the server...");
-        try {
-            System.in.read();
+       // Read the response from the web server
+       StringBuffer allresponse = new StringBuffer();
+       String response;
+       while ((response = in.readLine()) != null) {
+        allresponse.append(response+lineSeparator);
 
-            // Stop the mock web server
-            server1.stop();
-            server1.interrupt();
-            server2.stop();
-            server2.interrupt();
-            System.out.println("Mock web server stopped.");
-            System.exit(0);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+       }
 
+       Assert.assertEquals(TEST_RESPONSE, allresponse.toString());
+       
+       // Close the socket
+       socket.close();
+        // Assert that the response matches the expected response
     }
+
 
 }
